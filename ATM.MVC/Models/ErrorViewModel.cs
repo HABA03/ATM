fd6a77f632ab54f2d2f0f8c8fe8a8e0402141859
HABA03 <152206372+HABA03@users.noreply.github.com>
2025-03-13 15:20:36 -0600

    #1 Se creo la estructura del proyecto

---
+++ ATM.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ATM.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
